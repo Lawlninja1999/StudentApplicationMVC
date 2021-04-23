c974fe22475b5351feb5f0353f9b59f9d0fadafc
Lawlninja1999 <Armstrongteah96@outlook.com.au>
2021-04-23 20:20:28 +0800

    made changes

--- StudentApplicationMVC/Models/Teachers.cs
+++ StudentApplicationMVC/Models/Teachers.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,14 +11,19 @@ namespace StudentApplicationMVC.Models
     public class Teachers
     {
         public int TeachersID { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public int TeacherNumber { get; set; }
         public string AccessLevel { get; set; }
         public string TeacherPhoto { get; set; }
-        [NotMapped]
+        [NotMapped, Required]
         public IFormFile TeacherFormFile { get; set; }
         public virtual ICollection<UnitDetails> Units { get; set; }  
     }
